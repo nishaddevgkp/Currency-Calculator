8f516578eca3ffd37fd4fa79fdaeab57b2219959
vikram nishad <vikramnishaddev@gmail.com>
2024-10-13 09:35:17 -0400

    Initial Commit

---
+++ Currency Calculator/Currency_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Currency_CalculatorApp.swift
+//  Currency Calculator
+//
+//  Created by user256446 on 10/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Currency_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
